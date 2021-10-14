a5c003a5c932596fec0fcb821ff907481385819a
AlejandroHP96 <90903717+AlejandroHP96@users.noreply.github.com>
2021-10-14 13:23:48 +0100

    arreglar programa

--- buscarramplazar/src/main/java/dad/BuscarRemplazar.java
+++ buscarramplazar/src/main/java/dad/BuscarRemplazar.java
@@ -1,14 +1,23 @@
 package dad;
 
 import javafx.application.Application;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class BuscarRemplazar extends Application{
+public class BuscarRemplazar extends Application {
+
+    private TextField buscarTextField,remplazarTextField;
+    private CheckBox checkBox1,checkBox2,checkBox3,checkBox4;
+    
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        
+
+
+
     }
+
     public static void main(String[] args) {
         launch(args);
     }
