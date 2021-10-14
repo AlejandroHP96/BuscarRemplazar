b00063b83ed986d76ad3629b1febbfc132337ee5
AlejandroHP96 <90903717+AlejandroHP96@users.noreply.github.com>
2021-10-14 13:09:26 +0100

    Arreglando pom

--- buscarramplazar/src/main/java/dad/BuscarRemplazar.java
+++ buscarramplazar/src/main/java/dad/BuscarRemplazar.java
@@ -1,9 +1,15 @@
 package dad;
 
-/**
- * BuscarRemplazar
- */
-public class BuscarRemplazar  {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    
+public class BuscarRemplazar extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
